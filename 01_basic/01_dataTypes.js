020e61432240a3bcc3fd8a36243baadb5bad9fdc
Himanshee Ahuja <86192893+Hfrkf@users.noreply.github.com>
2025-04-09 07:10:52 +0000

    datatypes

---
+++ 01_basic/01_dataTypes.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) //we are using nodejs not browser
+console.log(3+
+    3  //code readability is high
+)
+let name="efeg"
+let age=12
+let loggedIn=false
+
+//number=>2 to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+//null=> standalone value
+//undeined=> no value is assigned yet
+//symbol=> unique
+
+//object
+console.log(typeof "fgrj");
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);//undefined
