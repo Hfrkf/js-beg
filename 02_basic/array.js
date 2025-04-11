597dab35f25fe87cb2251a11a0ef560527f67885
Himanshee Ahuja <86192893+Hfrkf@users.noreply.github.com>
2025-04-11 07:31:44 +0000

    array

---
+++ 02_basic/array.js
@@ -0,0 +1,38 @@
+//array
+const myArr=[0,1,2,6,"himanshee"]
+const myArr1=["shaktiman","doga"]
+const myArr2=new Array(1,2,3,4)
+console.log(myArr[1]);
+
+//+++++++++ array methods
+myArr.push(8)
+myArr.push(6)
+myArr.pop()
+myArr.unshift(8)
+myArr.shift()
+console.log(myArr);
+
+console.log(myArr.includes(7));//False
+console.log(myArr.indexOf(7));//-1
+console.log(myArr.indexOf(6));//3
+
+const newArr=myArr.join()
+console.log(myArr);//[0,1,2,6,...]
+console.log(newArr);//0,1,2,6,....
+console.log(typeof(newArr));//string
+
+//+++++++++ slice,splice +++++++++++++
+console.log("A",myArr);
+
+const myn1=myArr.slice(1,3)
+console.log(myn1);
+
+console.log("B",myArr);
+const myn2=myArr.splice(1,3)
+console.log(myn2);
+
+console.log("C",myArr);
+
+
+
+
