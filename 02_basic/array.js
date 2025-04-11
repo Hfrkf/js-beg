6cb807daf9c8b324c3fdcc4866f2a147102fd022
Himanshee Ahuja <86192893+Hfrkf@users.noreply.github.com>
2025-04-11 09:12:14 +0000

    array

--- 02_basic/array.js
+++ 02_basic/array.js
@@ -8,8 +8,8 @@ console.log(myArr[1]);
 myArr.push(8)
 myArr.push(6)
 myArr.pop()
-myArr.unshift(8)
-myArr.shift()
+myArr.unshift(8)//insert an element at first position
+myArr.shift()//deletes the first element
 console.log(myArr);
 
 console.log(myArr.includes(7));//False
@@ -25,13 +25,12 @@ console.log(typeof(newArr));//string
 console.log("A",myArr);
 
 const myn1=myArr.slice(1,3)
-console.log(myn1);
-
+console.log(myn1);//[1,2]
 console.log("B",myArr);
-const myn2=myArr.splice(1,3)
-console.log(myn2);
 
-console.log("C",myArr);
+const myn2=myArr.splice(1,3)
+console.log(myn2);//[1,2,6]
+console.log("C",myArr);//all elements of myn2 from the aray
 
 
 
