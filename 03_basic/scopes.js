faf9e77cef2f5542b35a38d536ce3f953356c58e
Himanshee Ahuja <86192893+Hfrkf@users.noreply.github.com>
2025-04-15 19:38:04 +0000

    scope

--- 03_basic/scopes.js
+++ 03_basic/scopes.js
@@ -10,9 +10,35 @@ if(true){//block scope
 }
 
 
-console.log(a);//error
-console.log(b);//error
-console.log(c);//return the value of c thus var is bekar to use 
+// console.log(a);//error
+// console.log(b);//error
+// console.log(c);//return the value of c thus var is bekar to use 
+
+
+//nested scope
+function one(){
+    const username="himanshee"
+    console.log(username);
+
+    function two(){
+        const website="github"
+        console.log(username);
+    }
+    //console.log(website);  //cant be assesed outide the function
+    two()
+}
+one()
+
+//same in if operator too
+if(true){
+    const username="himu"
+    if(username==="himu"){
+        const website="yoo"
+        console.log(username+website);//himu yoo
+    }
+    //console.log(website);//gives an error 
+}
+
 
 
 
