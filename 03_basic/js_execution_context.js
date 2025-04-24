2f94e1d41acac4db56ae28d910e56dafb1b049dc
Himanshee Ahuja <86192893+Hfrkf@users.noreply.github.com>
2025-04-24 18:35:14 +0000

    execution of code theory

---
+++ 03_basic/js_execution_context.js
@@ -0,0 +1,11 @@
+/*
+{}
+there are 3 execution context
+    global execution context
+    function execution context
+    eval execution context
+
+{}->runs under 2 phases
+    memory creation phase= memory allocation
+    execution phase= values assigned , all such operators performed +,-,.. 
+*/
