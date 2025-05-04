60b9514a16d1d5c81c00718ae9dd9a67a9b2952b
Himanshee Ahuja <86192893+Hfrkf@users.noreply.github.com>
2025-05-04 07:52:55 +0000

    switch

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,25 @@
+//switch
+/*
+switch(key){
+    case value:
+        break;
+    default:
+        break;
+}
+*/
+
+const month=3//"may"
+switch(month){
+    case 1://case "jan" and so on
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    default:
+        console.log("default value mtaches");
+        
+}
